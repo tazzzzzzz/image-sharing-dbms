def4305b9c13581bc77e15b5b7042890245759d7
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-05-31 02:12:27 +0530

    Scripts for inserting data into the relations.

---
+++ insert/insert_3tags.sql
@@ -0,0 +1,26 @@
+ALTER TABLE tags AUTO_INCREMENT = 1;
+
+INSERT INTO tags
+		(tag_name)
+	VALUES
+		('sunset'),
+		('photography'),
+		('sunrise'),
+		('landscape'),
+		('food'),
+		('foodie'),
+		('delicious'),
+		('beauty'),
+		('stunning'),
+		('dreamy'),
+		('lol'),
+		('happy'),
+		('fun'),
+		('style'),
+		('hair'),
+		('fashion'),
+		('party'),
+		('concert'),
+		('nature'),
+		('beach'),
+		('smile');
---
+++ insert/insert_4locations.sql
@@ -0,0 +1,28 @@
+ALTER TABLE location AUTO_INCREMENT = 1;
+
+
+INSERT INTO location
+		(location_name)
+	VALUES
+		('Manchester'),
+		('Palermo'),
+		('Bangalore'),
+		('Nairobi'),
+		('Brussels'),
+		('Berlin'),
+		('Helsinki'),
+		('Oslo'),
+		('Tokyo'),
+		('Moscow'),
+		('Stockholm'),
+		('Denver'),
+		('Bogota'),
+		('Marseille'),
+		('Rio'),
+		('Florence'),
+		('Caribbean'),
+		('London'),
+		('Jerusalem'),
+		('Barcelona'),
+		('Madrid');
+
